e263a24d3e4349a81f18eae16b21b724b006d296
bsipek07 <borna.sipek@gmail.com>
2025-03-11 18:35:39 +0100

    ovo je prvi commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static int Djelitelj(int n){
+
+        int dj=0;
+        for(int i=1;i<n;i++)
+        {
+            if(n%i==0)  dj=i;
+        }
+        return dj;
+    }
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1 ; i< 100; i++){
+            if(i % 2 == 0){
+                sum = sum  + i ;
+            }
+        }
+        for(int i=0;i<40;i++){
+            if(Djelitelj(i)<i*5)System.out.println(i);
+        }
+        System.out.println(sum);
+        System.out.println(dj5);
+    }
+}
