18a56cf1ab5948592726f945e3e9ea93718e120d
bsipek07 <borna.sipek@gmail.com>
2025-03-11 19:03:04 +0100

    multi funkcija

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,32 @@ public class Main {
         }
         return dj;
     }
+    public static int multi(int n){
+
+         int mul=0;
+        for(int i=1;i<n;i++){
+            if((n*i)>=(n+10))mul+=i;
+
+        }
+        return mul;
+    }
+
 
     public static void main(String[] args) {
         int sum = 0;
-        int dj5 = 0;
+
         for (int i = 1; i < 100; i++) {
             if (i % 4 == 0) {
                 sum = sum + i;
             }
         }
-        for (int i = 0; i < 40; i++) {
-            if (Djelitelj(i) < i * 5) System.out.println(i);
-        }
         System.out.println(sum);
-        System.out.println(dj5);
+        System.out.println(Djelitelj(sum));
+        System.out.println(multi(sum));
+            if (Djelitelj(sum) > multi(sum)) System.out.println("Djelitelj je veći");
+                    else System.out.println("Mnozitelj je veći");
+
     }
-}
+
+    }
+
